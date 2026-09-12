ff059dec335ed6499bd196fc8b93945ea4745f77
vuckoo0 <pedja.kg.1602@gmail.com>
2026-09-12 16:09:20 +0200

    Removed: Removed the account button from the nav section of the start page

--- Frontend/startPage.js
+++ Frontend/startPage.js
@@ -1,12 +1,7 @@
 const booksButton = document.querySelector('#books-button');
-const accountButton = document.querySelector('#account-button');
 
 booksButton.addEventListener('click', event => {
     window.location.href = 'books.html';
-})
-
-accountButton.addEventListener('click', event => {
-    window.location.href = 'login.html';
 })
 
 if (sessionStorage.getItem('isRunning') != '1') {
